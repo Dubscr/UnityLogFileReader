bc048c4e23768a73c33a41f3ddb67a4c218759ad
Dubscr <Dubscur@gmail.com>
2024-08-03 03:12:23 -0400

    All exceptions!!!!!!!!!!!!!!!!!!

--- UnityLogFileReader.py
+++ UnityLogFileReader.py
@@ -4,13 +4,44 @@ from enum import Enum
 import tkinter as tk
 from tkinter import ttk
 class DebugTypes(Enum):
-    UNDEFINED = 0
-    NULLREFERENCEEXCEPTION = 1
-    USERERRORLOG = 2
-    USERWARNINGLOG = 3
-    USERLOG = 4
+    UNDEFINED = 0,
+    SYSTEMEXCEPTION = 1,
+    SYSTEMSYSTEMEXCEPTION = 2,
+    ARGUMENTEXCEPTION = 3,
+    ARGUMENTNULLEXCEPTION = 4,
+    ARGUMENTOUTOFRANGEEXCEPTION = 5,
+    ARITHMETICEXCEPTION = 6,
+    ARRAYTYPEMISMATCHEXCEPTION = 7,
+    DIVIDEBYZEROEXCEPTION = 8,
+    FORMATEXCEPTION = 9,
+    INDEXOUTOFRANGEEXCEPTION = 10,
+    INVALIDCASTEXCEPTION = 11,
+    INVALIDOPERATIONEXCEPTION = 12,
+    NULLREFERENCEEXCEPTION = 13,
+    OUTOFMEMORYEXCEPTION = 14,
+    OVERFLOWEXCEPTION = 15,
+    STACKOVERFLOWEXCEPTION = 16,
+    TYPEINITIALIZATIONEXCEPTION = 17,
+    UNAUTHORIZEDACCESSEXCEPTION = 18,
+    IOEXCEPTION = 19,
+    FILENOTFOUNDEXCEPTION = 20,
+    DIRECTORYNOTFOUNDEXCEPTION = 21,
+    ENDOFSTREAMEXCEPTION = 22,
+    PATHTOOLONGEXCEPTION = 23,
+    UNITYEXCEPTION = 24,
+    MISSINGCOMPONENTEXCEPTION = 25,
+    MISSINGREFERENCEEXCEPTION = 26,
+    UNASSIGNEDREFERENCEEXCEPTION = 27,
+    PLAYERPREFSEXCEPTION = 28,
+    ASSETBUNDLECREATEREQUEST = 29,
+    ASSETBUNDLEREQUEST = 30,
+    SCENEMANAGEMENTEXCEPTION = 31,
+    EDITORAPPLICATIONEXCEPTION = 32,
+    BUILDFAILEDEXCEPTION = 33,
+    USERERRORLOG = 34,
+    USERWARNINGLOG = 35,
+    USERLOG = 36
 class SectionData(object):
-    logFile = ""
     debugMessage = ""
     scriptAndFunction = ""
     frequency = 0
@@ -29,10 +60,10 @@ class bcolors:
 thisDir = __file__.replace("\\", "/").replace(os.path.basename(__file__), "")
 logsFolder = thisDir + "/unitylogs/"
 
-logSectionScriptAndData = dict(script = str(), selectionData = SectionData)
-projectAssetsFolder = "C:/Users/Owner/Desktop/Github/Default/Untitled-Ghost-Game/Assets"
+logSectionScriptFromLogAndData = dict()
+projectAssetsFolder = "C:\\Users\\Owner\\Desktop\\Github\\Default\\Murder Mystery\\Assets"
 projectScripts = dict(script = str(), dir = str())
-
+scriptsToIgnore = ["UnityEngine", "UnityEditor"]
 errorsOnly = False
 
 # Gets all consecutive alphanumeric substrings in string
@@ -121,9 +152,12 @@ def GetDebugLogs(section):
     for i, line in enumerate(lines):
         if "Debug:Log" in line:
             data = SectionData()
-            data.debugMessage = lines[i - 2]
+            if(str(re.search(r'\S+\.\s*\S*\s*\(.*\)', section).group(0))):
+                data.debugMessage = lines[i - 2]
             if(len(lines) > i + 1):
-                data.scriptAndFunction = str(re.search(r'\b\w+:\w+\b(?=\(.*?\))', lines[i+1]).group(0))
+                result2 = re.search(r'\b\w+:\w+\b(?=\(.*?\))', lines[i+1])
+                if(result2 is not None):
+                    data.scriptAndFunction = str(result2.group(0))
                 return data
     return None
 
@@ -143,24 +177,93 @@ def GetScriptContents(file):
         if(contents != ""):
             return contents
         
-def GetSectionDebugType(content = ""):
-    if(content == ""):
-        return DebugTypes.UNDEFINED    
-    if("NullReferenceException" in content):
+def GetSectionDebugType(content=""):
+    if content == "":
+        print(f"Could not find debug type at content:\n{content}\nempty content??")
+        return DebugTypes.UNDEFINED
+    
+    if "NullReferenceException" in content:
         return DebugTypes.NULLREFERENCEEXCEPTION
-    elif("Debug:LogError" in content):
+    elif "ArgumentOutOfRangeException" in content:
+        return DebugTypes.ARGUMENTOUTOFRANGEEXCEPTION
+    elif "UnityException" in content:
+        return DebugTypes.UNITYEXCEPTION
+    elif "MissingComponentException" in content:
+        return DebugTypes.MISSINGCOMPONENTEXCEPTION
+    elif "MissingReferenceException" in content:
+        return DebugTypes.MISSINGREFERENCEEXCEPTION
+    elif "UnassignedReferenceException" in content:
+        return DebugTypes.UNASSIGNEDREFERENCEEXCEPTION
+    elif "SystemException" in content:
+        return DebugTypes.SYSTEMEXCEPTION
+    elif "SystemSystemException" in content:
+        return DebugTypes.SYSTEMSYSTEMEXCEPTION
+    elif "ArgumentException" in content:
+        return DebugTypes.ARGUMENTEXCEPTION
+    elif "ArgumentNullException" in content:
+        return DebugTypes.ARGUMENTNULLEXCEPTION
+    elif "ArithmeticException" in content:
+        return DebugTypes.ARITHMETICEXCEPTION
+    elif "ArrayTypeMismatchException" in content:
+        return DebugTypes.ARRAYTYPEMISMATCHEXCEPTION
+    elif "DivideByZeroException" in content:
+        return DebugTypes.DIVIDEBYZEROEXCEPTION
+    elif "FormatException" in content:
+        return DebugTypes.FORMATEXCEPTION
+    elif "IndexOutOfRangeException" in content:
+        return DebugTypes.INDEXOUTOFRANGEEXCEPTION
+    elif "InvalidCastException" in content:
+        return DebugTypes.INVALIDCASTEXCEPTION
+    elif "InvalidOperationException" in content:
+        return DebugTypes.INVALIDOPERATIONEXCEPTION
+    elif "OutOfMemoryException" in content:
+        return DebugTypes.OUTOFMEMORYEXCEPTION
+    elif "OverflowException" in content:
+        return DebugTypes.OVERFLOWEXCEPTION
+    elif "StackOverflowException" in content:
+        return DebugTypes.STACKOVERFLOWEXCEPTION
+    elif "TypeInitializationException" in content:
+        return DebugTypes.TYPEINITIALIZATIONEXCEPTION
+    elif "UnauthorizedAccessException" in content:
+        return DebugTypes.UNAUTHORIZEDACCESSEXCEPTION
+    elif "IOException" in content:
+        return DebugTypes.IOEXCEPTION
+    elif "FileNotFoundException" in content:
+        return DebugTypes.FILENOTFOUNDEXCEPTION
+    elif "DirectoryNotFoundException" in content:
+        return DebugTypes.DIRECTORYNOTFOUNDEXCEPTION
+    elif "EndOfStreamException" in content:
+        return DebugTypes.ENDOFSTREAMEXCEPTION
+    elif "PathTooLongException" in content:
+        return DebugTypes.PATHTOOLONGEXCEPTION
+    elif "AssetBundleCreateRequest" in content:
+        return DebugTypes.ASSETBUNDLECREATEREQUEST
+    elif "AssetBundleRequest" in content:
+        return DebugTypes.ASSETBUNDLEREQUEST
+    elif "SceneManagementException" in content:
+        return DebugTypes.SCENEMANAGEMENTEXCEPTION
+    elif "EditorApplicationException" in content:
+        return DebugTypes.EDITORAPPLICATIONEXCEPTION
+    elif "BuildFailedException" in content:
+        return DebugTypes.BUILDFAILEDEXCEPTION
+    elif "Debug:LogError" in content:
         return DebugTypes.USERERRORLOG
-    elif("Debug:LogWarning" in content):
+    elif "Debug:LogWarning" in content:
         return DebugTypes.USERWARNINGLOG
-    elif("Debug:Log" in content):
+    elif "Debug:Log" in content:
         return DebugTypes.USERLOG
+    
+    print(f"Could not find debug type at content:\n{content}")
     return DebugTypes.UNDEFINED
+
 
 def NonErrors(section, logFile, freq):
     userLogSectionData = SectionData()
-    userLogSectionData = GetDebugLogs(section)
-    if(userLogSectionData == None):
+    newData = GetDebugLogs(section)
+    if(newData is None):
         return
+    userLogSectionData.debugMessage = newData.debugMessage
+    userLogSectionData.scriptAndFunction = newData.scriptAndFunction
     userLogSectionData.logType = GetSectionDebugType(section)
             
     # Null reference exceptions will have the script name and function listed.
@@ -168,10 +271,9 @@ def NonErrors(section, logFile, freq):
         splitFunction = userLogSectionData.scriptAndFunction.split(":")
         scriptName = splitFunction[0]
         functionName = splitFunction[1]
-        userLogSectionData.scriptAndFunction = functionName 
-        userLogSectionData.logFile = logFile
+        userLogSectionData.scriptAndFunction = functionName
         userLogSectionData.frequency = freq
-        logSectionScriptAndData[scriptName] = userLogSectionData
+        logSectionScriptFromLogAndData[scriptName + ":" + logFile] = userLogSectionData
 
 def Errors(section, logFile, freq):
     for scriptAndFunction in GetScriptAndFunctionFromSection(section):
@@ -187,8 +289,7 @@ def Errors(section, logFile, freq):
         logSectionData.frequency = freq
         logSectionData.scriptAndFunction = functionName
         logSectionData.logType = GetSectionDebugType(section)
-        logSectionData.logFile = logFile
-        logSectionScriptAndData[scriptName] = logSectionData
+        logSectionScriptFromLogAndData[scriptName + ":" + logFile] = logSectionData
 def GetColorFromLogType(logType):
     if(logType == DebugTypes.NULLREFERENCEEXCEPTION):
         return bcolors.FAIL
@@ -200,15 +301,27 @@ def GetColorFromLogType(logType):
         return bcolors.OKCYAN
     else:
         return bcolors.ENDC
+def sort_key(item):
+    key, value = item
+    return key.split(":")[1]
+def ListToString(s):
 
+    # initialize an empty string
+    str1 = ""
+
+    # traverse in the string
+    for ele in s:
+        str1 += ele
+
+    # return string
+    return str1
 def StageOne():
     # Get list of valid Unity log files
     validatedUnityLogFiles = GetValidLogFiles()
     for file in validatedUnityLogFiles:
         # Get frequency of each section (Sorts out sections with less than 10 occurrences)
         for section, freq in GetFrequencyFromLogFile(file).items():
-            print(section + "\n" + "\n")
-            if(freq > 10):
+            if(freq > 6):
                 Errors(section, file, freq)
                 NonErrors(section, file, freq)
         
@@ -222,27 +335,34 @@ def StageTwo():
 def StageThree():
     fileName = ""
     # Remove empty script
-    logSectionScriptAndData.pop("script")
+    #logSectionScriptFromLogAndData.pop("script")
 
     # Sort dictionary. This is to make sure the files are ready one after another, instead of randomly switching
-    sorted_dict = dict(sorted(logSectionScriptAndData.items(), key=lambda item: item[1].logFile))
+    sorted_dict = dict(sorted(logSectionScriptFromLogAndData.items(), key=sort_key))
+    #sorted_dict = dict(sorted(logSectionScriptFromLogAndData.keys(), key=lambda key: key.split(":")[0]))
 
     # Loop through log file sections
-    for script, data in sorted_dict.items():
-        if(fileName != data.logFile):
-            print(f"{bcolors.HEADER}{bcolors.UNDERLINE}Reading File: { data.logFile }..." + bcolors.ENDC + "\n")
-            fileName = data.logFile
+    for scriptFromFile, data in sorted_dict.items():
+        if(fileName != scriptFromFile.split(":")[1]):
+            print(f"{bcolors.HEADER}{bcolors.UNDERLINE}Reading File: { scriptFromFile.split(":")[1] }..." + bcolors.ENDC + "\n")
+            fileName = scriptFromFile.split(":")[1]
         for file, dir in projectScripts.items():
             if(dir != ""):
-                if(script == file):
+                if(scriptFromFile.split(":")[0] == file):
                     print(f"{bcolors.OKGREEN}Reading Script: {file}" + bcolors.ENDC + "\n") 
-                    for i, lines  in enumerate(GetScriptContents(dir)):
-                        if(lines != ""):
-                            if(data.scriptAndFunction in lines):
-                                if(data.debugMessage != ""):
-                                    print(GetColorFromLogType(data.logType) + "User debug message: " + data.debugMessage)
-                                print(GetColorFromLogType(data.logType) + file + ": " + dir + " " + str(i + 1) + "\n" + "Log type: " + str(data.logType) + "\n" + bcolors.ENDC)
-                    print("Happened " + str(data.frequency) + " times.\n\n\n")
+                    if(data.debugMessage != ""):
+                        print(bcolors.ENDC + GetColorFromLogType(data.logType) + "Debug message: " + data.debugMessage + bcolors.ENDC)
+                    scriptContents = GetScriptContents(dir)
+                    scriptContentsString = ListToString(scriptContents)
+                    print(bcolors.ENDC + GetColorFromLogType(data.logType) + "Happened at function: " + data.scriptAndFunction + bcolors.ENDC)
+                    if(data.scriptAndFunction in scriptContentsString):
+                        for i, lines  in enumerate(scriptContents):
+                            if(lines != ""):
+                                if(data.scriptAndFunction in lines):
+                                    print(GetColorFromLogType(data.logType) + file + ": " + dir + " " + str(i + 1))
+                    else:
+                        print(bcolors.FAIL +"Could not find function in script from log. Usually because it is a coroutine." + bcolors.ENDC)
+                    print("\n" + "Log type: " + str(data.logType) + "\n\n" + bcolors.ENDC + "Happened " + str(data.frequency) + " times.\n\n\n")
 if __name__ == "__main__":
     StageOne()
     StageTwo()
